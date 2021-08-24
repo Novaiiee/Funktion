3b7302b5ff6c7b202f6740232ad8a12e57302c8d
TwitoCode <toheebeji@gmail.com>
2021-08-24 10:16:51 -0400

    Reverting back ( i can't stand css)

--- client/components/stories/Todo.stories.tsx
+++ client/components/stories/Todo.stories.tsx
@@ -1,13 +1,7 @@
 import { Meta, Story } from "@storybook/react";
 import { Todo, TodoProps } from "../Todo";
 
-const Template: Story<TodoProps> = (props) => (
-	<div className="grid md:grid-cols-3 md:grid-rows-3 sm:grid-rows-2 sm:grid-cols-2 gap-4 px-5 md:px-28 place-content-center">
-		{[...Array(9)].map((_, i) => {
-			return <Todo {...props} key={i} />;
-		})}
-	</div>
-);
+const Template: Story<TodoProps> = (props) => <Todo {...props} />;
 
 export const Default = Template.bind({});
 Default.args = {
